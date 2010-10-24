c870e0c8fe3ddb52465d17ed671ac960532c0218
Brian Jordan <bcjordan@gmail.com>
2010-10-24 15:46:43 -0400

    Working towards JS --> Rails

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,4 +2,13 @@ class DemoController < ApplicationController
   def index
   end
 
+# models/project.rb
+def self.search(search)
+  if search
+    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+  else
+    find(:all)
+  end
+end
+
 end
