4ea9458e714457ed79705fa3a6e01e78a97d4de8
Brian Jordan <bcjordan@gmail.com>
2010-10-24 14:46:59 -0400

    Initialize Rails, add index view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchBoxInference::Application.config.secret_token = '8bb7756cba5af89577762a277672631256316398e49568c8d1f90ff8e04dc434bb3f5c14e81fb85e33939122ae420032bec5c13332986ddd2cfd913d5e50d6f3'
